fee880b51f9c7ff65dbbb9cf25f17fec3b51f95d
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-09-22 13:30:01 +0200

    PlayerMove
    Le joueur peux désormais se déplacer en vue FPS dans un hub

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    private PlayerMove playerMove;
+
+    private void Start()
+    {
+        playerMove = GetComponentInParent<PlayerMove>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        playerMove.isGrounded = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        playerMove.isGrounded = false;
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float forceJump;
+    public float sensitivity;
+    private float headRotation;
+    public bool isGrounded;
+    
+    private Rigidbody rb;
+    public Transform cam;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
+        transform.Rotate(0f, x, 0f);
+        headRotation += y;
+        headRotation = Mathf.Clamp(headRotation, -90, 90);
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+
+        x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveBy = transform.right * x + transform.forward * z;
+        rb.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * forceJump * Time.deltaTime);
+        }
+
+        /*if (Input.GetKey(KeyCode.Z))
+        {
+            rb.velocity = Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.velocity = Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = Vector3.right * speed * Time.deltaTime;
+        }*/
+
+    }
+}
